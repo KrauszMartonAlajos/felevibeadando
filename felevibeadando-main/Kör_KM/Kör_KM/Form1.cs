656ef3dc49657c47820c9de8d0b166141be20654
KrauszMartonAlajos <77116668+KrauszMartonAlajos@users.noreply.github.com>
2024-01-05 15:21:56 +0100

    Update Form1.cs

--- felevibeadando-main/Kör_KM/Kör_KM/Form1.cs
+++ felevibeadando-main/Kör_KM/Kör_KM/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using System.Xml.Serialization;
 using System.IO;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static Kör_KM.Form1;
+using NUnit.Framework;
 
 namespace Kör_KM
 {
@@ -21,32 +23,42 @@ namespace Kör_KM
         public Form1()
         {
             InitializeComponent();
-
         }
 
+
+        /// <summary>
+        /// - Megrajzolja a koordináta rendszert
+        /// - Le tiltja a körök megrajzolásának gombját
+        /// - Meghívja az "adatfelvitel()" függvényt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
             panel1.Paint += KoordinataRendszerRajz;
             korok_rajz.Enabled = false;
-            adatfelvitel();
-            
+            adatfelvitel();            
         }
 
+        /// <summary>
+        /// Le ellenőrzi hogy van e beolvasható file és ezt jelzi a felhsaználónak
+        /// </summary>
         private void adatfelvitel()
         {
             try
             {
-                StreamReader r = new StreamReader("input.txt");
-                
+                StreamReader r = new StreamReader("input.txt");               
                 label1.ForeColor = Color.Green;
             }
             catch
             {
                 label1.ForeColor = Color.Red;
-
             }
         }
 
+        /// <summary>
+        /// Fájlbeolvasás
+        /// </summary>
         private void fajlbe()
         {
             StreamReader r = new StreamReader("input.txt");
@@ -59,18 +71,25 @@ namespace Kör_KM
             }
         }
 
+
+        /// <summary>
+        /// - Ki számolja az összes kör közös területét az átfedéséket is figyelembe véve
+        /// - Először össze adja az összes kör területét
+        /// - Majd egy ciklussal végig megy minden körön és kivonja azon körök metszetéből az átfedett területet melyeknek a sugarainak összege kisebb mint a két kör közti távolság
+        /// </summary>
+        /// <returns>Vissza adja az összesített területet (double típusban)</returns>
         public static double OsszesTerulet()
         {
             double OsszTerulet = 0.0;
 
             for (int i = 0; i < körök.Count; i++)
             {
-                double aktkot_terulet = Math.PI * Math.Pow(körök[i].r, 2);
-                OsszTerulet += aktkot_terulet;
+                double aktkor_terulet = Math.PI * Math.Pow(körök[i].r, 2);
+                OsszTerulet += aktkor_terulet;
 
                 for (int j = i + 1; j < körök.Count; j++)
                 {
-                    double tavketkorkozott = Math.Sqrt(Math.Pow(körök[j].X() - körök[i].X(), 2) + Math.Pow(körök[j].Y() - körök[i].Y(), 2));
+                    double tavketkorkozott = KetKorKoztiTav(körök[j], körök[i]);
 
                     if (tavketkorkozott < körök[i].r + körök[j].r)
                     {
@@ -82,6 +101,14 @@ namespace Kör_KM
             return OsszTerulet;
         }
 
+
+        /// <summary>
+        /// Ki számolja két kör metszetének területét
+        /// </summary>
+        /// <param name="r1">Az első kör sugara (double-ben megadva)</param>
+        /// <param name="r2">A második kör sugara (double-ben megadva)</param>
+        /// <param name="d">A két kör közti távolság (double-ben megadva)</param>
+        /// <returns></returns>
         private static double AtfedettTerulet(double r1, double r2, double d)
         {
             double d1 = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
@@ -93,6 +120,11 @@ namespace Kör_KM
 
         private static List<List<KOR>> korMatrix = new List<List<KOR>>();
 
+        /// <summary>
+        /// Egy mátrixba gyűjti a köröket amelyek érintik egymást majd ennek elemeit egy ListBoxba pakolja
+        /// </summary>
+
+        
         private void ErintosKorok()
         {
             korMatrix.Clear();
@@ -108,20 +140,56 @@ namespace Kör_KM
             }
         }
 
+        /// <summary>
+        /// A beküldött kört megnézi hány másik kör érinti és az összes esetet beküldi a fentebb említett mátrixba (korMatrix)
+        /// </summary>
+        /// <param name="kor">Az akruálisan vizsgált kör</param>
         private static void AktKorMitErint(KOR kor)
         {
             for (int i = 0; i < körök.Count; i++)
             {
-                double tav = Math.Sqrt(Math.Pow(körök[i].X() - kor.X(), 2) + Math.Pow(körök[i].Y() - kor.Y(), 2));
+                double tav = KetKorKoztiTav(körök[i],kor);
                 if (tav != 0 && (tav == (kor.r + körök[i].r)))
                 {
                     korMatrix.Add(new List<KOR>() { kor, körök[i] });
-
                 }
             }
         }
 
-        private void OrigotTartalmazoKorok()
+
+        /// <summary>
+        /// Két kör közti területet adja vissza (double-ben)
+        /// </summary>
+        /// <param name="k1">KOR osztály első vizsgált példánya</param>
+        /// <param name="k2">KOR osztály második vizsgált példánya</param>
+        /// <returns>távolság a két kör között (double-ben)</returns>
+        private static double KetKorKoztiTav(KOR k1, KOR k2)
+        {
+            return Math.Sqrt(Math.Pow(k1.X() - k2.X(), 2) + Math.Pow(k1.Y() - k2.Y(), 2));
+        }
+        [TestFixture]
+        public class KetKorKoztiTavTestClass
+        {
+            [Test]
+            public void TestKetKorKoztiTav()
+            {
+                // Arrange
+                KOR k1 = new KOR(0, 0,5,1); // Replace with actual values
+                KOR k2 = new KOR(3, 4,5,1); // Replace with actual values
+
+                // Act
+                double result = Form1.KetKorKoztiTav(k1, k2);
+
+                // Assert
+                Assert.AreEqual(5, result, 0.001); // Adjust the tolerance (0.001 in this case) based on your needs
+            }
+        }
+
+        /// <summary>
+        /// Megadja hány db kör tartalmazza magán belül az origót
+        /// </summary>
+        /// <returns>A feltételnek megfelelő körök számát</returns>
+        private int OrigotTartalmazoKorok()
         {
             int db = 0;
             for (int i = 0; i < körök.Count; i++)
@@ -131,12 +199,13 @@ namespace Kör_KM
                     db++;
                 }
             }
-            label3.Text = db.ToString() + " kör tartalmazza az Origót";
-
+            return db;
         }
 
 
-
+        /// <summary>
+        /// Megadja melyik kör van a legtávolabb az origó ponttól
+        /// </summary>
         private void OrigoMaxTav()
         {
             double kor = 0;
@@ -152,6 +221,9 @@ namespace Kör_KM
             label5.Text = String.Format(ssz + "-es sorszámú kör van a legtávolabb az origótól");
         }
 
+        /// <summary>
+        /// Minden vizsgált kört listáz a combo boxokba hogy a felhasználó ki tudja őket választani távolság mérésnél
+        /// </summary>
         private void ComboBoxbaPakol()
         {
             for (int i = 0; i < körök.Count; i++)
@@ -161,6 +233,11 @@ namespace Kör_KM
             }
         }
 
+        /// <summary>
+        /// Megrajzolja a koordináta rendszert
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void KoordinataRendszerRajz(object sender, PaintEventArgs e)
         {
             Graphics rajz = e.Graphics;
@@ -170,6 +247,14 @@ namespace Kör_KM
             rajz.DrawLine(tengely, 0, panel1.Height / 2, panel1.Width, panel1.Height / 2);
             rajz.DrawEllipse(origo, panel1.Width / 2 - 2, panel1.Height / 2 - 2, 4, 4);
         }
+
+        /// <summary>
+        /// Először eldönti hogy rajzolt körökkel lesz feltöltve a lista vagy generáltakkal
+        /// Aztán függvénymeghívások kerülnek ide
+        /// Majd megrajzolja a köröket egyesével a kor_rajzolas függvény segítségével
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void korok_rajz_Click(object sender, EventArgs e)
         {
             if (label2.BackColor == Color.DarkGreen)
@@ -184,18 +269,24 @@ namespace Kör_KM
             OrigoMaxTav();
             ErintosKorok();
             label4.Text = String.Format("A körök összesített Területe: " + Convert.ToString(OsszesTerulet()));
-            OrigotTartalmazoKorok();
+            label3.Text = OrigotTartalmazoKorok().ToString() + " kör tartalmazza az Origót";
             listBox1.Items.Clear();
 
             for (int i = 0; i < körök.Count; i++)
             {
                 kor_rajzolas(körök[i].X(), körök[i].Y(), körök[i].r, körök[i].sorszam);
-
                 listBox1.Items.Add(körök[i].ToString());
             }
             korok_rajz.Enabled = false;
-            //listBox2.Items.Clear();
         }
+
+        /// <summary>
+        /// Meg rajzolja az adott kört az adatai alapján
+        /// </summary>
+        /// <param name="x">a kör x koordinátája</param>
+        /// <param name="y">a kör y koordinátája</param>
+        /// <param name="r">a kör sugara</param>
+        /// <param name="sorszam">a kör sorszáma</param>
         private void kor_rajzolas(int x, int y, int r, int sorszam)
         {
             Graphics rajz = panel1.CreateGraphics();
@@ -211,15 +302,16 @@ namespace Kör_KM
 
         }
 
+        /// <summary>
+        /// Megadja lenyomásakor a két kör közti távolságot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
             try
             {
-                int k1x = körök[comboBox1.SelectedIndex].X();
-                int k1y = körök[comboBox1.SelectedIndex].Y();
-                int k2x = körök[comboBox2.SelectedIndex].X();
-                int k2y = körök[comboBox2.SelectedIndex].Y();
-                double tav = Math.Sqrt(Math.Pow(k2x - k1x, 2) + Math.Pow(k2y - k1y, 2));
+                double tav = KetKorKoztiTav(körök[comboBox1.SelectedIndex], körök[comboBox2.SelectedIndex]);
                 tavolsagaketkornek.Text = String.Format("A két kiválasztott kör távolsága: " + Convert.ToString(tav));
             }
             catch
@@ -229,11 +321,10 @@ namespace Kör_KM
 
         }
 
+
         private void button1_Click_1(object sender, EventArgs e)
         {
-
-            körök.Clear();
-            
+            körök.Clear();           
             MessageBox.Show("Adatok sikeresen felvéve");
         }
 
@@ -292,6 +383,12 @@ namespace Kör_KM
 
 
         static int s = 0;
+        // a gomb váltáshoz kell
+        /// <summary>
+        /// Ezzel vált vizuálisan a két lehetőség közül és változtatja azok színét
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
             körök.Clear();
@@ -309,6 +406,10 @@ namespace Kör_KM
                 s++;
             }
         }
+
+        /// <summary>
+        /// Megadott darab kört generál figyelve arra hogy ne csússzon ki egy se a koordináta rendszerből
+        /// </summary>
         private void KorGenRandom()
         {
             Random r = new Random();
@@ -331,6 +432,11 @@ namespace Kör_KM
             
         }
 
+        /// <summary>
+        /// Töröl minden háttér adatok és a kooridináta rendszert hogy új köröket rajzolhasson a felhasználó
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click_2(object sender, EventArgs e)
         {
             körök.Clear();
@@ -340,15 +446,25 @@ namespace Kör_KM
             panel1.Invalidate();
         }
 
+
+        /// <summary>
+        /// A lenyomásakor a kiválasztott kör adatai bekerülnek egy másik listBoxba
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            listBox3.Items.Clear();
-            listBox3.Items.Add("Sorszám: "+körök[listBox1.SelectedIndex].sorszam+".");
-            listBox3.Items.Add("Közép pontja: (" + körök[listBox1.SelectedIndex].X() + "," + körök[listBox1.SelectedIndex].Y() + ")");
-            listBox3.Items.Add("Sugár: " + körök[listBox1.SelectedIndex].r);
-            listBox3.Items.Add("Kerület: " + körök[listBox1.SelectedIndex].K());
-            listBox3.Items.Add("Terület: " + körök[listBox1.SelectedIndex].T());
-            listBox3.Items.Add("Távolság az origótól: " + körök[listBox1.SelectedIndex].tav);
+            try
+            {
+                listBox3.Items.Clear();
+                listBox3.Items.Add("Sorszám: " + körök[listBox1.SelectedIndex].sorszam + ".");
+                listBox3.Items.Add("Közép pontja: (" + körök[listBox1.SelectedIndex].X() + "," + körök[listBox1.SelectedIndex].Y() + ")");
+                listBox3.Items.Add("Sugár: " + körök[listBox1.SelectedIndex].r);
+                listBox3.Items.Add("Kerület: " + körök[listBox1.SelectedIndex].K());
+                listBox3.Items.Add("Terület: " + körök[listBox1.SelectedIndex].T());
+                listBox3.Items.Add("Távolság az origótól: " + körök[listBox1.SelectedIndex].tav);
+            }
+            catch { };
         }
     }
 }
